f6aad18fec6d5fd421bd19c31f309720aa7f006f
PtOlga <5441700@google.com>
2026-02-11 01:04:04 +0100

    fix: Update Plasmic project ID and token in plasmic-init.ts

--- src/plasmic-init.ts
+++ src/plasmic-init.ts
@@ -1,15 +1,17 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
 
 export const PLASMIC = initPlasmicLoader({
-  projects: [
-    {
-      id: "72gc4LrFpVVHVdH6iUBSQo",
-      token: "ECSoAYF7KU6XzLq3rBiHxARN1PIJAOZdvLr8YpyKPT5KO8Pddy1kHpllY2iqt8sm60v39GwCTh2YVjQzgOQ",
-    },
-  ],
+	projects: [
+	  {
+	    // New Plasmic project you just created
+	    id: "iddF84WskUGnwku9fPcTNt",
+	    token:
+	      "5AXeSgREZcHwe22memWa8RKmnMNOdp73OkSGlc39m0dlnPoo9Q2nBnIEhgUR1CABOxG8ozqwCvuvls3EW965yg",
+	  },
+	],
 
-  // By default Plasmic will use the latest published version of your project.
-  // For development you can set preview: true to see changes in real-time.
-  preview: true,
+	// Fetches the latest revisions, whether or not they were unpublished!
+	// Disable for production to ensure you render only published changes.
+	preview: true,
 });
 
