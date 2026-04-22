3d38592c9fa6a783466cbddb160bd9743c33f49a
PtOlga <5441700@google.com>
2026-04-22 20:57:01 +0200

    feat: simplify consent script integration in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import { DM_Sans } from 'next/font/google'
-import Script from 'next/script'
 import { getLocale } from 'next-intl/server'
 import './globals.css'
 import { DENIED_ANALYTICS_CONSENT } from '@/utils/consent'
 
 const dmsans = DM_Sans({ subsets: ['latin'] })
 const AHREFS_ANALYTICS_KEY = 'TwtKVhhnWtH5kaegiFlDwg'
+const CONSENT_DEFAULT_SCRIPT = `window.dataLayer=window.dataLayer||[];function gtag(){window.dataLayer.push(arguments);}window.gtag=window.gtag||gtag;window.gtag('consent','default',${JSON.stringify({ ...DENIED_ANALYTICS_CONSENT, wait_for_update: 500 })});`
 
 export default async function RootLayout({
   children,
@@ -18,18 +18,14 @@ export default async function RootLayout({
     <html lang={locale} suppressHydrationWarning>
       <head>
         <script
+          id='google-consent-default'
+          dangerouslySetInnerHTML={{ __html: CONSENT_DEFAULT_SCRIPT }}
+        />
+        <script
           async
           src='https://analytics.ahrefs.com/analytics.js'
           data-key={AHREFS_ANALYTICS_KEY}
         ></script>
-        <Script id='google-consent-default' strategy='beforeInteractive'>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window.dataLayer.push(arguments);}
-            window.gtag = window.gtag || gtag;
-            window.gtag('consent', 'default', ${JSON.stringify({ ...DENIED_ANALYTICS_CONSENT, wait_for_update: 500 })});
-          `}
-        </Script>
       </head>
       <body className={dmsans.className}>{children}</body>
     </html>
